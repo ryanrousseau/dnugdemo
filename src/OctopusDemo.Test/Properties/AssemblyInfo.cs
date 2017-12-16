363e41f7b706cd9d3a7a88f30bfa89f4ee1d3f53
Ryan Rousseau <ryan.rousseau@gmail.com>
2017-12-16 19:13:09 +0000

    Added tests

---
+++ src/OctopusDemo.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OctopusDemo.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OctopusDemo.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a313526a-796b-4f17-99fe-5c06b974b1cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/OctopusDemo.Test/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OctopusDemo.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+        }
+    }
+}
